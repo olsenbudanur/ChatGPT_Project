55921cfa643e32d8e29b7126446da4fe4c072ff4
olsenbudanur <olsenbudanur@gmail.com>
2022-12-25 15:31:33 -0500

    More setting up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState<string>('Waiting on value');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/college-essay')
+        .then((response) => response.json())
+        .then((data) => setValue(data.body));
+  }, []);
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type="textarea" 
+          name="textValue"
+        />
+        <div className="response">{value}</div>
     </div>
   );
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+const PORT = 8080;
 
-const port = 8080;
+app.use(bodyParser.json());
 
 app.get('/college-essay', (req, res) => {
-    res.send('Will serve the college essay here.')
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.json({body: 'Will serve the college essay here. '})
 })
 
 
-
-app.listen(port, () => {
-    console.log(`Server running on port: ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
 })
