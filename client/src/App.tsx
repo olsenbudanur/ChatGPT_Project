4c2317d945a0e16fd254dd63e38f088940d673d8
Niya Ma <niyama12321@gmail.com>
2022-12-25 20:25:25 -0500

    made api work

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,56 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import './App.css';
 
-function App() {
-  const [value, setValue] = useState<string>('Waiting on value');
 
-  function submit(){
-    fetch('http://localhost:8080/college-essay', {method: 'POST', body: JSON.stringify({a: "Data for ChatGPT", b: "Data for ChatGPT"})})
-        .then((response) => response.json())
-        .then((data) => {setValue(data.body)});
+
+function App() {
+  const [ChatGPTMessage, setChatGPTMessage] = useState<string>("");
+  const [value, setValue] = useState<string>("");
+
+
+  async function submit(){
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append('Accept', 'application/json');
+    myHeaders.append('Origin','http://localhost:3000');
+  
+  
+  var raw = JSON.stringify({
+    "ChatGPTMessage": ChatGPTMessage,
+  });
+  
+  var requestOptions: RequestInit = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+    mode: 'cors'
+  };
+  
+  fetch("http://localhost:8080/college-essay", requestOptions)
+    .then(response => response.json())
+    .then( data => setValue(data.body))
+    .catch(error => console.log('error', error));
   }
 
-  useEffect(() => {
-    
-  }, []);
+
+
+
+  
+
+
+  useEffect(() => {}, []);
 
   return (
     
     <div className="App">
         <input type="textarea" 
           name="textValue"
+          onChange={(e) => setChatGPTMessage(e.target.value)}
         />
         <button onClick={submit} >Send the data</button>
-        <br></br>
-        <br></br>
         <div>{value}</div>
         <br></br>
         <TypeAnimation 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,19 @@
-import express from 'express';
+import express, { Express, Request, Response } from "express";
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 const PORT = 8080;
 
 app.use(bodyParser.json());
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 // app.get('/college-essay', (req, res) => {
 //     res.header("Access-Control-Allow-Origin", "*");
@@ -13,10 +22,11 @@ app.use(bodyParser.json());
 // })
 
 
-app.post('/college-essay', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.json({body: 'Will serve the college essay here: ', req: req.body})
+app.post('/college-essay', (req: Request, res: Response) => {
+    console.log(req.body);
+    // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.json({body: req.body['ChatGPTMessage'], req: req.body})
 })
 
 
