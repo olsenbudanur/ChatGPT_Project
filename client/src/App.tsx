f5a35d49739a961c3ed3c412df4c910852860138
olsenbudanur <olsenbudanur@gmail.com>
2022-12-27 03:05:59 -0500

    Hayat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import TestingPage from "./pages/TestingPage";
+import TestingPage from "./pages/A4";
 import Navbar from "./components/Navbar/Navbar";
+import Prompt from "./pages/Prompt";
 import Footer from "./components/Footer/Footer";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <Router>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/test" element={<TestingPage />} />
-      </Routes>
-      {/* <Footer></Footer> */}
-    </Router>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/test" element={<TestingPage />} />
+				<Route path="/prompt" element={<Prompt />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
