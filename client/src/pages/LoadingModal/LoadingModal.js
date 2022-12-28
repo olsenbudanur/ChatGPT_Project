93d19e9fdfe20fa649d5310ea35e267726747b1c
olsenbudanur <olsenbudanur@gmail.com>
2022-12-27 20:45:32 -0500

    Writing animation

---
+++ client/src/pages/LoadingModal/LoadingModal.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import * as S from "./LoadingModal.styles";
+import "./LoadingModal.css";
+
+const style = {
+	position: "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	width: "50%",
+	bgcolor: "#262626",
+	border: "2px solid #000",
+	boxShadow: 24,
+	p: 4,
+	height: "50%",
+};
+
+export default function BasicModal() {
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+
+	return (
+		<div>
+			<Button onClick={handleOpen}>Open modal</Button>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<Box sx={style}>
+					<body>
+						<div class="stroke"></div>
+					</body>
+				</Box>
+			</Modal>
+		</div>
+	);
+}
