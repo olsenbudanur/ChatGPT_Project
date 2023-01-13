84b23f2e783e2611f6a4428e668fe0bd38912bb3
olsenbudanur <olsenbudanur@gmail.com>
2023-01-12 19:54:19 -0500

    Push

--- server/src/server.ts
+++ server/src/server.ts
@@ -207,7 +207,7 @@ app.get("/hayat", async (req: Request, res: Response) => {
 
 app.use(express.static(path.resolve(__dirname, "../../client/build")));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(
     path.join(__dirname, "../", "../", "client", "build", "index.html")
   );
