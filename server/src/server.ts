263bd21fe2cc4646ecfc9eaa65c248a075ec7f66
olsenbudanur <olsenbudanur@gmail.com>
2023-01-12 20:20:23 -0500

    Hi

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,12 +19,18 @@ const app = express();
 const PORT = 8080;
 app.use(bodyParser.json());
 const corsOptions = {
-  origin: true,
+  origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 app.options("*", cors());
+
+app.use((req, res, next) => {
+  res.set("Access-Control-Expose-Headers", "location");
+  res.set("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 //
 // Set up stripe
@@ -138,8 +144,6 @@ async function consultOpenAI(prompt: string): Promise<string> {
 //
 // The post request for writing a college essay.
 app.post("/college-essay", async (req: Request, res: Response) => {
-  res.set("Access-Control-Expose-Headers", "location");
-  res.set("Access-Control-Allow-Credentials", "true");
   try {
     const email = req.body.email;
 
